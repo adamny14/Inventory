1468efe346404d59ca4d632ca15124f6cbe20595
Adam Hussain <adamahussain528@gmail.com>
2019-10-27 16:40:16 -0400

    Added code to multithread uploading information to DB

--- Invintory/LoadData.py
+++ Invintory/LoadData.py
@@ -1,39 +1,52 @@
 #File to move data from txt file to postgress db
 import psycopg2
 from config import config
+from multiprocessing import Pool
+import time
+from itertools import product
 
-def connect():
-    conn = None
-    try:
+class DBData:
+    def __init__(self):
         params = config()
 
         print("Connecting to PostgreSQL database...")
-        conn = psycopg2.connect(**params)
+        self.conn = psycopg2.connect(**params)
 
-        cur = conn.cursor()
+        self.cur = self.conn.cursor()
+        print("\x1b[1;32m"+"Connection Successful \x1b[1;0m")
+        self.filepath = input("Enter the path of the file to import from: ")
 
-        print("PostgreSQL database version: ")
-        cur.execute("SELECT * from \"Products\"")
+        print("Beginning Uploading data to DB")
+        self.start=time.time()
+        
+    def connect(self):
+        try:                                
+            self.ReadFile(self.filepath)
+            print("Finished uploading data to DB: ",time.time()-self.start)
+            self.cur.close()
+        except(Exception, psycopg2.DatabaseError) as error:
+            print(error)
 
-        db_version = cur.fetchone()
-        print(db_version)
+    def ReadFile(self, filepath):
+        if filepath=="":
+            print("Missing file path")
+            return
+        
+        file=open(self.filepath, "r")
+        pool=Pool()
+        results=pool.map(self, ReadLines, [line for line in file])
+        
 
-        cur.close()
-    except(Exception, psycopg2.DatabaseError) as error:
-        print(error)
+    def ReadLines(self, line):
+        lineArr = line.split()
+        sql="Insert into \"Products\"(\"Name\", \"Location\", \"Date_Purchased\", \"Quantity\", \"Type\", \"Photo\", \"Color\", \"Company\")"
+        sql+=" VALUES(%s,%s,%s,%s,%s,%s,%s,%s)"
+        self.cur.execute(lineArr[1],lineArr[2],lineArr[3],lineArr[4],lineArr[5],psycopg2.Binary(lineArr[6]),lineArr[7],lineArr[8])
+        self.conn.commit()
 
-def ReadFile():
-    filepath = input("Enter the path of the file to import from: ")
-    if filepath=="":
-        print("Missing file path")
-        return
-    
-    file=open(filepath, "r")
-    for line in file:
-        col = line.split()
-        print(col[0])
-
+        
 if __name__ =='__main__':
-    connect()
-    ReadFile()
+    db = DBData()
+    db.connect()
 
+#C:\Users\adama\Documents\invintory.txt
